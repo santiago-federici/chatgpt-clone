73b9d1c7be048ba36720ba94b8f03b3c0662b86f
santiago-federici <santiagofederici88@gmail.com>
2024-09-13 19:10:05 -0300

    feat: add icons

---
+++ src/icons/plus.tsx
@@ -0,0 +1,19 @@
+export function PlusIcon({ className }: { className?: string }) {
+  return (
+    <svg
+      className={className}
+      width="24"
+      height="24"
+      viewBox="0 0 24 24"
+      fill="none"
+      stroke="currentColor"
+      strokeWidth="2"
+      strokeLinecap="round"
+      strokeLinejoin="round"
+    >
+      <path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
+      <path d="M12 5l0 14"></path>
+      <path d="M5 12l14 0"></path>
+    </svg>
+  );
+}
