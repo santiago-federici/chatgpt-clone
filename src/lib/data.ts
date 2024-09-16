483df6441c8ee3e986a41af3d3e8e133e8b9dd8a
santiago-federici <santiagofederici88@gmail.com>
2024-09-16 09:10:38 -0300

    feat: create get functions

--- src/lib/data.ts
+++ src/lib/data.ts
@@ -1,5 +1,23 @@
-import { conversations } from "../constants/index";
+export async function getAllConversations() {
+  try {
+    const res = await fetch("http://127.0.0.1:8080/conversations");
+    const data = await res.json();
 
-export function getConversations() {
-  return conversations;
+    return data;
+  } catch (err) {
+    console.log("Error al obtener las conversaciones: ", err);
+  }
+}
+
+export async function getConversation(conversation_id: string) {
+  try {
+    const res = await fetch(
+      `http://127.0.0.1:8080/conversation/${conversation_id}`
+    );
+    const data = await res.json();
+
+    return data;
+  } catch (err) {
+    console.log("Error al obtener la conversacion: ", err);
+  }
 }
